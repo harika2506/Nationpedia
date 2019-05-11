d29e62e05b6b0b12bf23b188638e972d7c1a31de
Harika <billuriharika2506@gmail.com>
2019-05-10 20:03:20 -0700

    app.js change

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-/**
- * http://usejsdoc.org/
- */
-
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var lineReader = require('line-reader');
 var index = require('./app_server/routes/index');
 var app = express();
-var fs = require('fs');
-var mongoose = require("mongoose");
-var nodemailer = require("nodemailer");
-mongoose.Promise = global.Promise;
+
 
 //View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session( {secret: "String for encrypting cookies." } ));
-
-
-
-
-
+app.use(session( {
+    secret: "String for encrypting cookies.",
+    resave: true,
+} ));
 
 app.use('/', index);
 
 module.exports = app;
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
